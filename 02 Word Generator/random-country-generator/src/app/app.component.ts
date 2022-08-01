9b2fad2ac8c27162cf95b763e97fee442920c46a
SushantP199 <sushantpagam199@gmail.com>
2022-08-01 19:34:36 +0530

    word generator & random country selector apps completed

---
+++ 02 Word Generator/random-country-generator/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import countries from 'src/utils/countries';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'random-country-generator';
+
+  randomCountries: string[] = [];
+
+  randomNumber = 0;
+
+  generateRandomNumber(){
+    this.randomNumber = Math.floor(Math.random() * (countries.length) + 1);
+
+    this.selectRandomCountries();
+  }
+
+  selectRandomCountries(){
+    this.randomCountries = [];
+
+    while(this.randomCountries.length != this.randomNumber && this.randomCountries.length < this.randomNumber){
+      
+      var randomIndex = Math.floor(Math.random() * this.randomNumber);
+
+      if (!this.randomCountries.includes(countries[randomIndex])){
+        this.randomCountries.push(countries[randomIndex]);
+      }
+
+    }
+  }
+}
---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app-sushant",
+    // template: `
+    // <div>
+    //     <marquee scrollamount="30"><h1>Angular</h1></marquee>
+    // </div>
+    // `,
+    templateUrl: "./app.component.html",
+    // styles: [`
+    // div {
+    //     background-color: #fff;
+    //     width: 100vw;
+    //     padding: 1vh 0;
+    //     box-shadow: 2px 2px 2px #7B7A72;
+    // }
+
+    // h1 {
+    //     color: #DE2C30;
+    //     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    //     font-size: 100px;
+    //     text-align: center;
+    //     text-transform: uppercase;
+    // }
+    // `]
+    styleUrls: [
+        "./app.component.css"
+    ]
+})
+export class AppComponent {
+
+}
